cb0ab2e8d23018977e888570c6dfdf53d0ee7770
inmo <inmoy@naver.com>
2022-06-05 10:16:02 +0900

    JAVA TPC

--- JAVA/Java TPC/src/TPC04.java
+++ JAVA/Java TPC/src/TPC04.java
@@ -1,4 +1,4 @@
-public class TCP04 {
+public class TPC04 {
     public static void main(String[] args) {
         int a, b, c;
         a = 10;
--- JAVA/hongong_JAVA/src/App.java
+++ JAVA/hongong_JAVA/src/App.java
@@ -1,5 +1,17 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws IOException {
+        // 생활코딩 OOP 정리
+        System.out.println(Math.PI);
+        FileWriter f1 = new FileWriter("data.txt");
+        f1.write("Hello Java");
+
+        FileWriter f2 = new FileWriter("data2.txt");
+        f2.write("Hello Java2");
+        f1.write("!!!");
+        f1.close();
+        f2.close();
     }
 }
