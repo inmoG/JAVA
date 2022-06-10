3e291685ef389807434b299303e4eba5dc3f8fa1
inmoG <50260620+inmoG@users.noreply.github.com>
2022-06-10 14:43:54 +0900

    object-array

--- JAVA/JAVA_TPC/src/TPC18.java
+++ JAVA/JAVA_TPC/src/TPC18.java
@@ -27,10 +27,10 @@ public class TPC18 {
         // 영화 3편 저장을 위해 객체 배열 생성
         MovieVO[] Movie_arr = new MovieVO[3];
 
-        // 데이터 할당
+        // MovieVO자료형을 가진 각각의 영화 객체를 인덱스에 할당 해 영화를 저장한다. 따라서 MovieVO 하나의 자료형을 가진 배열이다.
         Movie_arr[0] = new MovieVO("범죄도시", 12000, "마동석", 15, 2.2f);
-        Movie_arr[1] = new MovieVO("범죄도시", 12000, "마동석", 15, 2.2f);
-        Movie_arr[2] = new MovieVO("범죄도시", 12000, "마동석", 15, 2.2f);
+        Movie_arr[1] = new MovieVO("범죄도시2", 12000, "마동석", 15, 2.3f);
+        Movie_arr[2] = new MovieVO("범죄도시3", 12000, "마동석", 15, 2.4f);
 
         for (int i = 0; i < Movie_arr.length; i++) {
             System.out.println(Movie_arr[i].toString());
