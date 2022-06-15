65b708ed30a96b408b19dcf42e2c40ed1c912e92
inmoG <50260620+inmoG@users.noreply.github.com>
2022-06-15 23:14:45 +0900

    Java API

---
+++ JAVA/JAVA_TPC/src/TPC33.java
@@ -0,0 +1,11 @@
+// import java.lang.*
+
+public class TPC33 {
+    // String
+    public static void main(String[] args) {
+        String str = new String("Apple");
+        str = str.toLowerCase();
+        System.out.println(str);
+        System.out.println(str.charAt(3));
+    }
+}
