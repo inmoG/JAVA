62bea493c0b8b6e35e7679f9d3b51307b9c03e09
inmoG <50260620+inmoG@users.noreply.github.com>
2022-09-22 02:38:36 +0900

    enum

--- JAVA/JAVA_TPC/src/godofjava/c/javapackage/enums/OverTimeValues2.java
+++ JAVA/JAVA_TPC/src/godofjava/c/javapackage/enums/OverTimeValues2.java
@@ -1,17 +1,18 @@
-package c.enums;
+package godofjava.c.javapackage.enums;
 
 public enum OverTimeValues2 {
     THREE_HOUR(18000),
     FIVE_HOUR(30000),
     WEEKEND_FOUR_HOUR(40000),
     WEEKEND_EIGHT_HOUR(60000);
+
     private final int amount;
-    OverTimeValues2(int amount)
-    {
+
+    OverTimeValues2(int amount) {
         this.amount = amount;
     }
-    public int getAmount()
-    {
+
+    public int getAmount() {
         return amount;
     }
 }
