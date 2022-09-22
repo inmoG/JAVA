f7b22b7379f66f1438d9af33a21181d8d1578f9f
inmoG <50260620+inmoG@users.noreply.github.com>
2022-09-22 14:21:08 +0900

    enum

---
+++ JAVA/JAVA_TPC/src/c/enums/PowerSwitch.java
@@ -0,0 +1,29 @@
+package c.enums;
+
+public enum PowerSwitch {
+    ON("켜짐"),
+    OFF("꺼짐");
+
+    private String krName;
+
+    private PowerSwitch() {
+        // constructor
+    }
+
+    private PowerSwitch(String krName) {
+        this.krName = krName;
+    }
+
+    public String getKrName() {
+        return krName;
+    }
+
+    public PowerSwitch opposite() {
+        if (this == PowerSwitch.ON) {
+            return PowerSwitch.OFF;
+        } else {
+            return PowerSwitch.ON;
+        }
+    }
+
+}
