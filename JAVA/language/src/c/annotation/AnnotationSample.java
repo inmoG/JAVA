1a86644dc0a6ed9b967bea04e47f898d096dfc66
inmo <inmoy@naver.com>
2022-10-01 13:41:16 +0900

    annotation

---
+++ JAVA/language/src/c/annotation/AnnotationSample.java
@@ -0,0 +1,9 @@
+package c.annotation;
+
+public class AnnotationSample {
+    @SuppressWarnings("deprecation")
+    public void useDeprecated() {
+        AnnotationOverride child = new AnnotationOverride();
+        child.noMoreUse();
+    }
+}
